098981bc57fbb950f99c97beb3d01b2909f0cd77
Hunter Bowman <huntercbowman@icloud.com>
2022-08-10 18:04:01 -0700

    Initial Commit

---
+++ Tally Watch App/TallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TallyApp.swift
+//  Tally Watch App
+//
+//  Created by Hunter Bowman on 8/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Tally_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
